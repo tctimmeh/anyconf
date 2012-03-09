dd2f4982b15df6c72e258347e05673df646e182f
Tim Court <tctimmeh@gmail.com>
2012-03-09 09:08:39 -0700

    child elements now available as properties, item index, and contains

--- src/anyconf/formats/xml/xmlConfig.py
+++ src/anyconf/formats/xml/xmlConfig.py
@@ -1,27 +1,18 @@
-from anyconf.configSection import ConfigSection
 from ...config import Config
+from .xmlConfigSection import XmlConfigSection
 from ...formats import FORMAT_XML
 
 import xml
 
-class XmlConfig(Config):
+class XmlConfig(Config, XmlConfigSection):
   def __init__(self):
     super(XmlConfig, self).__init__(FORMAT_XML)
 
   def loadFromFile(self, inputFile):
     self.parser = xml.dom.minidom.parse(inputFile)
 
-  def __getattr__(self, attributeName):
-    if self.parser.documentElement.nodeName == attributeName:
-      return ConfigSection()
-    raise AttributeError('Internal Error')
+  def _getChild(self, name):
+    if self.parser.documentElement.nodeName == name:
+      return XmlConfigSection(self.parser.documentElement)
+    return None
 
-  def __getitem__(self, attributeName):
-    if self.parser.documentElement.nodeName == attributeName:
-      return ConfigSection()
-    raise AttributeError('Internal Error')
-
-  def __contains__(self, attributeName):
-    if self.parser.documentElement.nodeName == attributeName:
-      return True
-    return False
---
+++ src/anyconf/formats/xml/xmlConfigSection.py
@@ -0,0 +1,28 @@
+import xml
+from ...configSection import ConfigSection
+
+class XmlConfigSection(ConfigSection):
+  def __init__(self, element):
+    super(XmlConfigSection, self).__init__()
+    self.element = element
+
+  def __getattr__(self, item):
+    return self.__getChildOrRaise(item, AttributeError)
+
+  def __getitem__(self, item):
+    return self.__getChildOrRaise(item, IndexError)
+
+  def __contains__(self, item):
+    return self._getChild(item) is not None
+
+  def __getChildOrRaise(self, item, exceptionType):
+    out = self._getChild(item)
+    if out is not None:
+      return out
+    raise exceptionType('No child named [%s]' % item)
+
+  def _getChild(self, name):
+    for child in self.element.childNodes:
+      if isinstance(child, xml.dom.minidom.Element) and (child.nodeName == name):
+        return XmlConfigSection(child)
+    return None
